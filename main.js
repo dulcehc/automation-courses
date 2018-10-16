8d42782fe1c6ccb1ddc35bb63287d20a6ba2d8cd
Dulce Hernandez <dulce.hernandezc@gmail.com>
2018-10-16 15:14:41 -0500

    settimeout animation

--- main.js
+++ main.js
@@ -6,17 +6,26 @@ function animation (e) {
   if (gearIcon.classList.contains('rollIn')) {
     gearIcon.classList.remove('rollIn')
     gearIcon.classList.add('rollOut')
-    hidden.map( btn => {
+
+    setTimeout(function(){
+     hidden.map( btn => {
       btn.classList.remove('btn-display');
       btn.classList.add('btn-hidden');
-    })
+    });
+  }, 5000)
   } else {
     gearIcon.classList.remove('rollOut')
     gearIcon.classList.add('rollIn')
-    hidden.map( btn => {
+    setTimeout(function(){
+      hidden.map( btn => {
+        btn.classList.remove('btn-hidden');
+        btn.classList.add('btn-display');
+      });
+    }, 5000);
+   /*  hidden.map( btn => {
       btn.classList.remove('btn-hidden');
       btn.classList.add('btn-display');
-    })
+    }) */
   }
 }
 gearIcon.addEventListener('click', animation);
