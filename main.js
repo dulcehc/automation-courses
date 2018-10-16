22b4e55356cd2372f41b9be6a8ac3833e44e2f24
Dulce Hernandez <dulce.hernandezc@gmail.com>
2018-10-15 20:16:15 -0500

    rollIn and rollOut animation for gear icon

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
 const gearIcon = document.querySelector('.icon-gear');
 const hidden = [...document.querySelectorAll('.btn-hidden')];
 
-function animation () {
+function animation (e) {
   console.log('clicked');
-  gearIcon.style.animationName = 'gears';
-  gearIcon.style.animationDuration = '10s';
-  gearIcon.style.animationFillMode = 'forwards';
-  hidden.map( btn => {
+  if (gearIcon.classList.contains('rollIn')) {
+    gearIcon.classList.remove('rollIn')
+    gearIcon.classList.add('rollOut')
+    hidden.map( btn => {
+      btn.classList.remove('btn-display');
+      btn.classList.add('btn-hidden');
+    })
+  } else {
+    gearIcon.classList.remove('rollOut')
+    gearIcon.classList.add('rollIn')
+    hidden.map( btn => {
       btn.classList.remove('btn-hidden');
       btn.classList.add('btn-display');
-  })
-
+    })
+  }
 }
 gearIcon.addEventListener('click', animation);
