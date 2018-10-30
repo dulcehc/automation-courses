3c822264a827845e20997c3954bc7eb6ffb8952e
Dulce Hernandez <dulce.hernandezc@gmail.com>
2018-10-29 23:57:55 -0600

    fix animation rollIN

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
 const gearIcon = document.querySelector('.icon-gear');
 const hidden = [...document.querySelectorAll('.btn-hidden')];
 
-function animation (e) {
-  console.log('clicked');
+function animation () {
   if (gearIcon.classList.contains('rollIn')) {
     gearIcon.classList.remove('rollIn')
     gearIcon.classList.add('rollOut')
 
     setTimeout(function(){
-     hidden.map( btn => {
-      btn.classList.remove('btn-display');
-      btn.classList.add('btn-hidden');
-    });
-  }, 5000)
+      hidden.map( btn => {
+        btn.classList.remove('btn-display');
+        btn.classList.add('btn-hidden');
+        btn.style.animationName = 'hideButtons';
+
+      });
+    }, 5000)
   } else {
     gearIcon.classList.remove('rollOut')
     gearIcon.classList.add('rollIn')
