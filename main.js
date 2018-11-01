b49f9bec7b497f6f077622ee760608bfe3206484
Dulce Hernandez <dulce.hernandezc@gmail.com>
2018-10-31 19:20:40 -0600

    increase speed of animation and remove unused code

--- main.js
+++ main.js
@@ -6,28 +6,20 @@ function animation () {
     gearIcon.classList.remove('rollIn')
     gearIcon.classList.add('rollOut')
 
-    setTimeout(function(){
-      hidden.map( btn => {
-        btn.classList.remove('btn-display');
-        //btn.classList.add('btn-hidden');
-        btn.classList.add('hideButtons');
-        //btn.style.animationName = 'hideButtons';
-
-      });
-    }, 1000)
+    hidden.map( btn => {
+      btn.classList.remove('btn-display');
+      btn.classList.add('hideButtons');
+    });
   } else {
     gearIcon.classList.remove('rollOut')
     gearIcon.classList.add('rollIn')
-    setTimeout(function(){
-      hidden.map( btn => {
-        btn.classList.remove('btn-hidden');
-        btn.classList.add('btn-display');
-      });
-    }, 2000);
-   /*  hidden.map( btn => {
+
+    hidden.map( btn => {
       btn.classList.remove('btn-hidden');
       btn.classList.add('btn-display');
-    }) */
+    });
+
   }
 }
+
 gearIcon.addEventListener('click', animation);
