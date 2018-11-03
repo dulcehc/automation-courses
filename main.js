1fa4c7289da8522ab5e3a254d61b1cc937347c58
Dulce Hernandez <dulce.hernandezc@gmail.com>
2018-11-02 21:46:59 -0600

    draw line animation

--- main.js
+++ main.js
@@ -4,20 +4,28 @@ const hidden = [...document.querySelectorAll('.hidden')];
 
 function animation () {
   let x = document.getElementById("temario");
+  let line = document.querySelector('.line');
+
   if (gearIcon.classList.contains('rollIn')) {
     gearIcon.classList.remove('rollIn')
     gearIcon.classList.add('rollOut')
+    /* line.classList.remove('drawLine'); */
+    line.classList.add('hideLine');
 
     /* hidden.map( btn => {
       btn.classList.remove('show');
       btn.classList.add('hidden');
     }); */
-    x.style.display = "none";
+     x.style.display = "none";
+    x.classList.add('hideTemario');
   } else {
     gearIcon.classList.remove('rollOut')
     gearIcon.classList.add('rollIn')
 
+    line.classList.remove('hideLine');
+    line.classList.add('drawLine');
     x.style.display = "block";
+
 
    /*  hidden.map( btn => {
       btn.classList.remove('hidden');
